75b53d0a6d23472577b56877b9f4692d92b863c6
DouaeMoeniss <dmoeniss@emeal.nttdata.com>
2025-12-21 01:26:58 +0100

    annulation 48 h

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Planification de la tâche d'annulation automatique des réservations babysitter (toutes les heures)
+// Assurez-vous que le cron est configuré sur le serveur (php artisan schedule:run)
+\Illuminate\Support\Facades\Schedule::command('babysitter:cancel-expired-bookings')->hourly();
